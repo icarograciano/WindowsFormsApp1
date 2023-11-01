e9a0fe956c4f5503fb832b0eeb37a8835daa1e43
icarograciano <icaro.graciano@gmail.com>
2023-11-01 19:46:07 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.checkBox5 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 62);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(430, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "a) Apresentar todos os valores numéricos inteiros ímpares situados na faixa de 0 " +
+    "a 40.";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 85);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(596, 17);
+            this.checkBox2.TabIndex = 2;
+            this.checkBox2.Text = "b) Apresentar o total da soma dos 100 primeiros números inteiros, representados p" +
+    "ela sequência: 1+2+3+4...+98+99+100.";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 108);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(539, 17);
+            this.checkBox3.TabIndex = 3;
+            this.checkBox3.Text = "c) Apresentar todos os números divisíveis por 4 que sejam menores que 40. Iniciar" +
+    " a contagem com o valor 1";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(12, 131);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(318, 17);
+            this.checkBox4.TabIndex = 4;
+            this.checkBox4.Text = "d) Apresentar os quadrados dos números inteiros de 15 a 200.";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // checkBox5
+            // 
+            this.checkBox5.AutoSize = true;
+            this.checkBox5.Location = new System.Drawing.Point(12, 154);
+            this.checkBox5.Name = "checkBox5";
+            this.checkBox5.Size = new System.Drawing.Size(501, 17);
+            this.checkBox5.TabIndex = 5;
+            this.checkBox5.Text = "e) Apresentar o resultado da soma de todos os valores pares existentes na faixa n" +
+    "umérica de 1 a 50. ";
+            this.checkBox5.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Executar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.25F);
+            this.label1.Location = new System.Drawing.Point(7, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(432, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Selecione uma opção e clique em Executar.\r\n";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 248);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Resultado";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(900, 538);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox5);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Desenvolvimento de soluções em C#";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.CheckBox checkBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string resultado = ""; // Esta variável conterá o resultado
+
+            if (checkBox1.Checked)
+            {
+                resultado += "a) Valores ímpares de 0 a 40:\n";
+                for (int i = 0; i <= 40; i++)
+                {
+                    if (i % 2 != 0)
+                    {
+                        resultado += i + ", ";
+                    }
+                }
+                resultado = resultado.TrimEnd(',', ' ') + "\n\n";
+            }
+
+            if (checkBox2.Checked)
+            {
+                int somaB = 0;
+                for (int i = 1; i <= 100; i++)
+                {
+                    somaB += i;
+                }
+                resultado += "b) Soma dos 100 primeiros números inteiros: " + somaB + "\n\n";
+            }
+
+            if (checkBox3.Checked)
+            {
+                resultado += "c) Números divisíveis por 4 menores que 40:\n";
+                for (int i = 1; i < 40; i++)
+                {
+                    if (i % 4 == 0)
+                    {
+                        resultado += i + ", ";
+                    }
+                }
+                resultado = resultado.TrimEnd(',', ' ') + "\n\n";
+            }
+
+            if (checkBox4.Checked)
+            {
+                resultado += "d) Quadrados dos números de 15 a 200:\n";
+                for (int i = 15; i <= 200; i++)
+                {
+                    resultado += (i * i) + ", ";
+                    if (i % 10 == 4) // Adiciona uma quebra de linha a cada 10 números
+                    {
+                        resultado += "\n";
+                    }
+                }
+                resultado = resultado.TrimEnd(',', ' ') + "\n\n";
+            }
+
+            if (checkBox5.Checked)
+            {
+                int somaE = 0;
+                for (int i = 2; i <= 50; i += 2)
+                {
+                    somaE += i;
+                }
+                resultado += "e) Soma dos valores pares de 1 a 50: " + somaE + "\n\n";
+            }
+
+            // Atualize a label2 com o resultado
+            label2.Text = resultado;
+        }
+
+
+    }
+}
